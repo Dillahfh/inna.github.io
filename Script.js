f227cef591d17f2f9f19b4c0477f95abde43e63e
Dillahfh <dillahldhyaa@gmail.com>
2025-01-09 20:09:29 +0800

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,111 @@
+
+let navbar = document.querySelector('.navbar');
+if (navbar) {
+    document.querySelector('#menu-bar').onclick = () => {
+        navbar.classList.toggle('active');
+    };
+}
+
+
+let search = document.querySelector('.search');
+if (search) {
+    document.querySelector('#search').onclick = () => {
+        search.classList.toggle('active');
+    };
+}
+
+var swiper = new Swiper(".product-row", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        dalay:9500,
+       disableOnInteraction: false, 
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1024: {
+        slidesPerView: 3,
+        
+      },
+    },
+  });
+
+  var swiper = new Swiper(".blogs-row", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        dalay:9500,
+       disableOnInteraction: false, 
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+      nextE1:".swiper-button-next",
+      prevE1:".swiper-button-prev",
+
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 1,
+        
+      },
+      1024: {
+        slidesPerView: 1,
+        
+      },
+    },
+  });
+
+ 
+var swiper = new Swiper(".review-row", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop:true,
+  centeredSlides:true,
+  autoplay:{
+      dalay:9500,
+     disableOnInteraction: false, 
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      
+    },
+    768: {
+      slidesPerView: 2,
+      
+    },
+    1024: {
+      slidesPerView: 3,
+      
+    },
+  },
+});
